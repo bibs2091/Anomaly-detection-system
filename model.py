f11b617dccad3555355c1cdaf84b4d93ebce96cb
Bond Benz <n.benzeghli@esi-sba.dz>
2020-07-04 06:52:52 +0100

    fixed converting to numpy

--- model.py
+++ model.py
@@ -5,5 +5,5 @@ import numpy as np
 
 model = pickle.load(open('decision_tree_model.pickle','rb'))
 df = pd.read_csv("ktest0.csv")
-to_predict = df.drop(['Label'], axis=1)
+to_predict = df.drop(['Label'], axis=1).to_numpy()
 print(model.predict(to_predict))
