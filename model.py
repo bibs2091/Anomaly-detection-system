9fe4ef10da2d1390b17bd9c8375c5ca9b2a30573
Bond Benz <n.benzeghli@esi-sba.dz>
2020-07-04 06:49:59 +0100

    pushing trained model dt

---
+++ model.py
@@ -0,0 +1,9 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+
+model = pickle.load(open('decision_tree_model.pickle','rb'))
+df = pd.read_csv("ktest0.csv")
+to_predict = df.drop(['Label'], axis=1)
+print(model.predict(to_predict))
