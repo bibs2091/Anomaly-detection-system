71140db4583a6b793f0c87b4537deb8ab9e5883f
bibs2091 <m.naas@esi-sba.dz>
2020-07-05 04:40:22 +0100

    add accuracy score function,change the format of the model to joblib

--- model.py
+++ model.py
@@ -1,25 +1,29 @@
-import pickle
 import pandas as pd
 import numpy as np
 import logging
+import sklearn
+from joblib import load
+import sys
+import warnings
+# if not sys.warnoptions:
+#     warnings.simplefilter("ignore")
 
 class model:
     def __init__(self):
         # load the pretrained model 
         try:
-            self.model = pickle.load(open('decision_tree_model.pickle','rb'))
+            self.model = load('decision_tree_model.joblib')
         except:
-            # error if model can't be found in the path
-            logging.error("Model can\'t be found in the main directory")
-            logging.error("please fix the problem and restart the server")
+             # error if model can't be found in the path
+             logging.error("Model can\'t be found in the main directory")
+             logging.error("please fix the problem and restart the server")
 
         # load the features for the preprocessing step
         try:
             features_file = open("features.txt", "r")
             self.features = []
             for feature in features_file:
-                self.features.append(feature)
-            print("1")
+                self.features.append(feature.strip())
         except:
             # error if features file can't be found in the path
             logging.error("features.txt can\'t be found in the main directory")
@@ -33,18 +37,38 @@ class model:
         data = data.dropna()
         #change the type of the data to float
         data = data.astype("float")
-        #remove the column with std equal to zero
-        data = data.loc[:, data.std() > 0]
         #return the data as numpy array
         return data.to_numpy()
 
     def load_data(self,path = 'data_examples/example.csv'):
         #load and preprocess the csv file
         self.data = pd.read_csv(path)
+        #for evaluation tasks, we will save the label
+        if ('Label' in self.data.columns):
+            self.label = self.data['Label'].to_numpy()
+        else:
+            self.label = None
+            logging.info('This data is labeled')
+
         self.data = self.preprocess(self.data)
 
     def predict(self):
-        self.prediction = model.predict(self.data)
+        #predict the class of the flow
+        self.prediction = self.model.predict(self.data)
         return self.prediction
 
-model()
+    def accuracy(self):
+        if (self.label is None):
+            logging.error("Score can't be calculated, No label provided")
+            logging.error("be sure to name your label column with 'Lebel'")
+            return None
+        else:
+            from sklearn.metrics import accuracy_score
+            accuracy = accuracy_score(self.label, self.prediction)
+            return accuracy
+
+
+m = model()
+m.load_data()
+print(m.predict())
+print(m.accuracy())
