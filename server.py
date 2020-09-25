d3bf49f235654fc8786e81498581c4d7d6f39312
ilovax <tasfaoutabderrahimilovax@gmail.com>
2020-09-25 14:06:43 +0100

    remove unwanted bytes from uncmonning flow

--- server.py
+++ server.py
@@ -3,6 +3,15 @@ from model import model
 
 m = model()
 
+def check_flow_return_string(recv):
+    allowed_chars = ["0","1","2","3","4","5","6","7","8","9",'.',',','-','N','e','d','M','a','n','u','l','L','b','E']
+    data =  ""
+    for i in recv:
+        char = chr(i)
+        if char in allowed_chars:                
+            data += chr(i)
+    return data
+
 def server_program():
     
     host = socket.gethostname()
@@ -16,7 +25,12 @@ def server_program():
     print("Connection from: " + str(address))
     while True:
         # won't accept data packet greater than 2048 bytes
-        data = conn.recv(2048).decode()
+
+        recv = conn.recv(2048)
+        data = check_flow_return_string(recv)
+        print(data)
+        #data = data.decode()
+        #data = conn.recv(2048).decode()
         if not data:
             # if data is not received break
             break
